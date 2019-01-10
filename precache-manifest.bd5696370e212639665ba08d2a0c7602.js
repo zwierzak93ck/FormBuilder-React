8d3520dbd5d1c8c92fa35b382cf0865c40993f0a
KapitanKebab <zwierzak93ck@gmail.com>
2019-01-10 22:51:49 +0100

    Updates

---
+++ precache-manifest.bd5696370e212639665ba08d2a0c7602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "342438937c11f168ade0",
+    "url": "/nazwa/static/js/runtime~main.34243893.js"
+  },
+  {
+    "revision": "0f2e8baf37abf642c41b",
+    "url": "/nazwa/static/js/main.0f2e8baf.chunk.js"
+  },
+  {
+    "revision": "a73aba86aa6858e44706",
+    "url": "/nazwa/static/js/1.a73aba86.chunk.js"
+  },
+  {
+    "revision": "0f2e8baf37abf642c41b",
+    "url": "/nazwa/static/css/main.663be296.chunk.css"
+  },
+  {
+    "revision": "a183b07ffd76a7d63291e4f15f048e57",
+    "url": "/nazwa/index.html"
+  }
+];
