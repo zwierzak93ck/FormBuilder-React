552e0bab2f1f4a2c16d7ac5e0207d7e2427a0141
KapitanKebab <zwierzak93ck@gmail.com>
2019-01-25 02:08:47 +0100

    Updates

--- precache-manifest.78490c2fc18d00acfc27b4b9d0a861cf.js
+++ precache-manifest.78490c2fc18d00acfc27b4b9d0a861cf.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/FormBuilder-React/static/js/runtime~main.176c3c20.js"
   },
   {
-    "revision": "66f6ae88a23ac0251bb8",
-    "url": "/FormBuilder-React/static/js/main.66f6ae88.chunk.js"
+    "revision": "a52758d03070b6bdc619",
+    "url": "/FormBuilder-React/static/js/main.a52758d0.chunk.js"
   },
   {
     "revision": "2c276626f2494adf5d8e",
     "url": "/FormBuilder-React/static/js/1.2c276626.chunk.js"
   },
   {
-    "revision": "66f6ae88a23ac0251bb8",
+    "revision": "a52758d03070b6bdc619",
     "url": "/FormBuilder-React/static/css/main.4b35222f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/FormBuilder-React/static/css/1.03384071.chunk.css"
   },
   {
-    "revision": "947a7407fe4835f852743ade46524a00",
+    "revision": "39ffbba14f31b5983f538cd2ed0fca95",
     "url": "/FormBuilder-React/index.html"
   }
 ];
