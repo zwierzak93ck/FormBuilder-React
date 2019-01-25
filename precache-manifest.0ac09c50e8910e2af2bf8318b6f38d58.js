740e138461e24285881ff1ad7d141d72e01a52b5
KapitanKebab <zwierzak93ck@gmail.com>
2019-01-25 07:49:09 +0100

    Updates

--- precache-manifest.0ac09c50e8910e2af2bf8318b6f38d58.js
+++ precache-manifest.0ac09c50e8910e2af2bf8318b6f38d58.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/FormBuilder-React/static/js/runtime~main.176c3c20.js"
   },
   {
-    "revision": "b70be9441383164afee0",
-    "url": "/FormBuilder-React/static/js/main.b70be944.chunk.js"
+    "revision": "f52f95b315ceeb5915ae",
+    "url": "/FormBuilder-React/static/js/main.f52f95b3.chunk.js"
   },
   {
     "revision": "2c276626f2494adf5d8e",
     "url": "/FormBuilder-React/static/js/1.2c276626.chunk.js"
   },
   {
-    "revision": "b70be9441383164afee0",
+    "revision": "f52f95b315ceeb5915ae",
     "url": "/FormBuilder-React/static/css/main.4b35222f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/FormBuilder-React/static/css/1.03384071.chunk.css"
   },
   {
-    "revision": "2331b1a174e97c26176e0f3874039918",
+    "revision": "dc6320a8c7ddbc5d9f6c3a72cfcb66a0",
     "url": "/FormBuilder-React/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/FormBuilder-React/precache-manifest.c6bb36285b8aeea205be6b0ecb4cf0af.js"
+  "/FormBuilder-React/precache-manifest.0ac09c50e8910e2af2bf8318b6f38d58.js"
 );
 
 workbox.clientsClaim();
